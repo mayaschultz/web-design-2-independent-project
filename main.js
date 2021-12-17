e680d221be0f0a11d820cfa631e03bf538e77e25
mayaschultz <mayasc98@gmail.com>
2021-12-16 20:17:13 -0500

    Add external javascript file

---
+++ main.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', (event) => {
+  document.getElementById("contact-now").addEventListener('click', function () {
+     location.href = "#";
+  })
+  document.getElementById("learn-more").addEventListener('click', function () {
+     location.href = "./pages/about.html";
+  })
+});
